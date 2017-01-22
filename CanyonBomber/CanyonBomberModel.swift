609c1942ec26fcdf3e4b05898a3326ccb4f98a7c
lowella garque <welaii@hotmail.com>
2017-01-22 15:39:06 -0500

    added classes and UIView of the game

---
+++ CanyonBomber/CanyonBomberModel.swift
@@ -0,0 +1,9 @@
+//
+//  CanyonBomberModel.swift
+//  CanyonBomber
+//
+//  Created by welai on 2017-01-22.
+//  Copyright © 2017 lactobacilai. All rights reserved.
+//
+
+import Foundation
---
+++ CanyonBomber/CanyonBomberVC.swift
@@ -0,0 +1,35 @@
+//
+//  CanyonBomberVC.swift
+//  CanyonBomber
+//
+//  Created by welai on 2017-01-22.
+//  Copyright © 2017 lactobacilai. All rights reserved.
+//
+
+import UIKit
+
+class CanyonBomberVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
